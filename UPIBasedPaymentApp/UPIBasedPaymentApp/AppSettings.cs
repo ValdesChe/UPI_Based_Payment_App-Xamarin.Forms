1339d2958090848d1f3626be4c08691cefa0f225
Valdes Che <valdesche03@gmail.com>
2020-12-22 22:07:28 +0100

    Created AppSettings file for app config

---
+++ UPIBasedPaymentApp/UPIBasedPaymentApp/AppSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UPIBasedPaymentApp
+{
+    public static class AppSettings
+    {
+        public const string IsFirstTimeAppLaunched = "IsFirstTimeAppLaunched";
+        public const bool IsFirstTimeAppLaunchedDefaultValue = true;
+
+    }
+}
