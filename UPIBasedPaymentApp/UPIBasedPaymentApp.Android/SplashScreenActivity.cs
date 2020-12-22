f4d6e2a6526163d0f4661cbfb72610b33efe0b5b
Valdes Che <valdesche03@gmail.com>
2020-12-22 20:45:08 +0100

    Added splash screen activity

---
+++ UPIBasedPaymentApp/UPIBasedPaymentApp.Android/SplashScreenActivity.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+
+namespace UPIBasedPaymentApp.Droid
+{
+    [Activity(Theme = "@style/MyTheme.Splash", MainLauncher = true, NoHistory = true)]
+    public class SplashScreenActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+        }
+
+        // Launches the startup task
+        protected override void OnResume()
+        {
+            base.OnResume();
+            Task startupWork = new Task(() => { SimulateStartup(); });
+            startupWork.Start();
+        }
+
+        // Simulates background work that happens behind the splash screen
+        void SimulateStartup()
+        {
+            StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+        }
+    }
+}
