735072936bdefcdf074d33c8d02671bb1a8cd89f
Valdes Che <valdesche03@gmail.com>
2020-12-22 18:22:04 +0100

    Integrate prism unity library

--- UPIBasedPaymentApp/UPIBasedPaymentApp.Android/MainActivity.cs
+++ UPIBasedPaymentApp/UPIBasedPaymentApp.Android/MainActivity.cs
@@ -6,6 +6,8 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Prism.Ioc;
+using Prism;
 
 namespace UPIBasedPaymentApp.Droid
 {
@@ -21,13 +23,20 @@ namespace UPIBasedPaymentApp.Droid
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
-            LoadApplication(new App());
+            LoadApplication(new App(new AndroidInitializer()));
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+    public class AndroidInitializer : IPlatformInitializer
+    {
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
         }
     }
 }
--- UPIBasedPaymentApp/UPIBasedPaymentApp/App.xaml.cs
+++ UPIBasedPaymentApp/UPIBasedPaymentApp/App.xaml.cs
@@ -1,28 +1,82 @@
-﻿using System;
+﻿using Prism.Unity;
+using Prism.Ioc;
+using Prism;
+using Prism.Navigation;
+using System;
+using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using UPIBasedPaymentApp.ViewModel;
 
 namespace UPIBasedPaymentApp
 {
-    public partial class App : Application
+    public partial class App : PrismApplication
     {
-        public App()
+        public App() : base(null) { }
+        public App(IPlatformInitializer initializer = null) : base(initializer, setFormsDependencyResolver: true)
+        {
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            // Register Services
+            
+            // Register Navigation Pages + ViewModels
+            containerRegistry.RegisterForNavigation<NavigationPage>();
+            containerRegistry.RegisterForNavigation<MainPage, MainPageViewModel>();
+
+        }
+
+        protected override async void OnInitialized()
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            await SetMainPageAsync();
         }
 
-        protected override void OnStart()
+        private async Task SetMainPageAsync()
         {
+            var isFirstTimeLaunched = true;
+            INavigationResult result = null;
+            if (isFirstTimeLaunched)
+            {
+            }
+
+            result = await NavigationService.NavigateAsync($"{nameof(NavigationPage)}/{nameof(MainPage)}");
+
+            if (!result.Success)
+            {
+                SetMainPageFromException(result.Exception);
+            }
         }
 
-        protected override void OnSleep()
-        {
-        }
 
-        protected override void OnResume()
+        private void SetMainPageFromException(Exception ex)
         {
+            var layout = new StackLayout
+            {
+                Padding = new Thickness(40)
+            };
+            layout.Children.Add(new Label
+            {
+                Text = ex?.GetType()?.Name ?? "Unknown Error encountered",
+                FontAttributes = FontAttributes.Bold,
+                HorizontalOptions = LayoutOptions.Center
+            });
+
+            layout.Children.Add(new ScrollView
+            {
+                Content = new Label
+                {
+                    Text = $"{ex}",
+                    LineBreakMode = LineBreakMode.WordWrap
+                }
+            });
+
+            MainPage = new ContentPage
+            {
+                Content = layout
+            };
         }
     }
 }
