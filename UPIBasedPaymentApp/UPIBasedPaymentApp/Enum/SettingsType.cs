3431b105daa6b76ed376d2c8287f45d5ee6ceb3a
Valdes Che <valdesche03@gmail.com>
2020-12-29 18:17:03 +0100

    Create  SettingsPageListItem class and enum

---
+++ UPIBasedPaymentApp/UPIBasedPaymentApp/Enum/SettingsType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UPIBasedPaymentApp.Enum
+{
+    public enum SettingsType : byte
+    {
+        PROFILE_EDIT = 1,
+        PASSWORD_EDIT = 2,
+        LANGUAGE_EDIT = 3,
+        TERMS_COND_EDIT = 4,
+    }
+}
---
+++ UPIBasedPaymentApp/UPIBasedPaymentApp/Views/Settings/SettingsPageListItem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UPIBasedPaymentApp.Enum;
+
+namespace UPIBasedPaymentApp.Views.Settings
+{
+    public class SettingsPageListItem
+    {
+        private string _icon;
+        private string _name;
+
+        public SettingsType? Type { get; set; }
+
+        public string Name
+        {
+            get => _name;
+            set { _name = value; }
+        }
+
+
+        public string Icon
+        {
+            get => _icon;
+            set { _icon = value; }
+        }
+    }
+}
