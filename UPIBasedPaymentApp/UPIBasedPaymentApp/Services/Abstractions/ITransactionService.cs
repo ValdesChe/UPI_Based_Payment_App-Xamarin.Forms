fb68fb0de5314ec876262cbc25ae3a1ccc27af1c
Valdes Che <valdesche03@gmail.com>
2021-01-02 20:34:42 +0100

    Services/Abstraction: create ITransactionService get all method

---
+++ UPIBasedPaymentApp/UPIBasedPaymentApp/Services/Abstractions/ITransactionService.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using UPIBasedPaymentApp.Models;
+
+namespace UPIBasedPaymentApp.Services.Abstractions
+{
+    public interface ITransactionService
+    {
+        /// <summary>
+        /// Fetch the transactions
+        /// </summary>
+        /// <returns></returns>
+        Task<IList<Transaction>> GetTransactions();
+    }
+}
