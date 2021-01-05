7782b66c28dea1f82f346f951fc21a3dd3ff13c6
Valdes Che <valdesche03@gmail.com>
2021-01-05 20:52:44 +0100

    Created transactions list data template selector

---
+++ UPIBasedPaymentApp/UPIBasedPaymentApp/Views/Selectors/TransactionTemplateSelector.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UPIBasedPaymentApp.Models;
+using Xamarin.Forms;
+
+namespace UPIBasedPaymentApp.Views.Selectors
+{
+    public class TransactionTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate CreditTemplate { get; set; }
+        public DataTemplate DebitTemplate { get; set; }
+        protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+        {
+            if (item is Transaction transactionItem)
+            {
+                return transactionItem.Type == Enum.TransactionType.CREDIT ? CreditTemplate : DebitTemplate;
+            }
+            return null;
+        }
+    }
+}
