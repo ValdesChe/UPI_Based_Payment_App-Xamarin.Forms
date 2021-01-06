f9a10feb25b4951fc65596960228afa5f6e02cf6
Valdes Che <valdesche03@gmail.com>
2021-01-06 12:02:40 +0100

    Entity: Created TransactionItemGroup entity

---
+++ UPIBasedPaymentApp/UPIBasedPaymentApp/Models/TransactionItemGroup.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace UPIBasedPaymentApp.Models
+{
+    public class TransactionItemGroup: ObservableCollection<Transaction>
+    {
+        public string Name { get; private set; }
+
+        public TransactionItemGroup(string name): base()
+        {
+            Name = name;
+        }
+
+        public TransactionItemGroup(string name, 
+            IEnumerable<Transaction> source) : base(source)
+        {
+            Name = name;
+        }
+    }
+}
